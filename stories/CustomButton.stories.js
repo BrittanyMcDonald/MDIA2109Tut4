3c950f79fa9e4ab885dd88c31cd2a0e75a96a585
Brittany McDonald <bmcdonald80@my.bcit.ca>
2020-04-05 16:21:07 -0700

    pushing new changes.. cancel click not working?

--- stories/CustomButton.stories.js
+++ stories/CustomButton.stories.js
@@ -7,3 +7,21 @@ export default {
 };
 
 export const MyCustomButton = () => <CustomButton />;
+
+export const PageWithCustomButtons = () => <div>
+    <CustomButton 
+        color="#999"
+        text="Cancel" />
+    <CustomButton
+        color="#3F5"
+        text="OK" />
+    <CustomButton 
+        color="#F3F"
+        text="Submit" />
+    <CustomButton 
+        text="Menu" />
+</div>
+
+function CancelClick(){
+    alert("cancel");
+}
